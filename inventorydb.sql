c21908d8fbb41eea58c3bd2defffeba8f9ddba55
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-03-19 17:17:45 +0300

    update BD

--- inventorydb.sql
+++ inventorydb.sql
@@ -1,15 +1,13 @@
--- CREATE DATABASE IF NOT EXISTS Inventory;
--- CREATE USER IF NOT EXISTS 'dev'@'localhost' IDENTIFIED BY 'dev123';
--- GRANT ALL PRIVILEGES ON Inventory.* TO 'dev'@'localhost';
--- GRANT SELECT ON performance_schema.* TO 'dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS Inventory;
+CREATE USER IF NOT EXISTS 'dev'@'localhost' IDENTIFIED BY 'dev123';
+GRANT ALL PRIVILEGES ON Inventory.* TO 'dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'dev'@'localhost';
 
--- USE Inventory;
--- CREATE TABLE IF NOT EXISTS users (
---     Email VARCHAR(255) PRIMARY KEY,
---     password VARCHAR(255) NOT NULL,
---     role VARCHAR(20) NOT NULL
--- );
+USE Inventory;
+CREATE TABLE IF NOT EXISTS users (
+    Email VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL
+);
 
--- FLUSH PRIVILEGES;
-
-SHOW DATABASE
+FLUSH PRIVILEGES;
