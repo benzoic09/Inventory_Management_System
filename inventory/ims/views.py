77c565c1b6cd9d5717f1c7d80af11bf86c1eb6ec
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-03-21 17:09:35 +0300

    working on login (ikona some errors)

--- inventory/ims/views.py
+++ inventory/ims/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
     return render (request, 'index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index.html')  # Replace 'home' with your desired URL name
+    else:
+        form = AuthenticationForm()
+    return render(request, 'index.html', {'login_form': form})
