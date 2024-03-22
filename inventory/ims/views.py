0c283ead54af2d33e84654335e1583069d8c383e
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-03-22 17:16:20 +0300

    fixed the login form

--- inventory/ims/views.py
+++ inventory/ims/views.py
@@ -4,16 +4,24 @@ from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
-    return render (request, 'index.html')
-
-
-def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('index.html')  # Replace 'home' with your desired URL name
+            return redirect('index')  # Redirect to index page after successful login
     else:
         form = AuthenticationForm()
     return render(request, 'index.html', {'login_form': form})
+
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('index')  # Use the URL name instead of the URL path
+#     else:
+#         form = AuthenticationForm()
+#     return render(request, 'index.html', {'login_form': form})
+ 
