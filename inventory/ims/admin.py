ce19a274987813e13215865d33a91543f2b3cef5
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-03-23 19:56:16 +0300

    updated models

--- inventory/ims/admin.py
+++ inventory/ims/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import devicetype, devices, employees, branch
 
 # Register your models here.
+admin.site.register(devicetype)
+admin.site.register(devices)
+admin.site.register(employees)
+admin.site.register(branch)
--- inventory/ims/models.py
+++ inventory/ims/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class devicetype(models.Model):
@@ -9,18 +10,41 @@ class devicetype(models.Model):
     date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.model
     
-class Devices(models.Model):
+class devices(models.Model):
     MacAddress=models.CharField(max_length=100,blank=True, null=True)
     SerialNo=models.CharField(max_length=250,blank=True, null=True) #this can be blank
     MobileNo = models.TextField()
-    Model = models.ForeignKey(devicetype)
+    Model = models.ForeignKey(devicetype, on_delete=models.PROTECT)
+    ScreenProtector = models.BooleanField(default=False)
+    Cover = models.BooleanField(default=False)
     status = models.CharField(max_length=2, choices=(('1','Active'),('2','Damaged'),('3','Dead')), default=1)
     date_created = models.DateTimeField(default=timezone.now)
     date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.MacAddress + ' - ' + self.name # to reseaech more on this 
-    
-    
+        return self.MacAddress + ' - ' + self.Model # to reseaech more on this 
+
+
+class branch(models.Model):
+    branch= models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.branch  
+
+
+class employees(models.Model):
+    empno = models.CharField(max_length=30)
+    Name = models.CharField(max_length=250)
+    Department = models.CharField(max_length=250)
+    branch = models.ForeignKey(branch, on_delete=models.PROTECT)
+    BusinessUnit = models.CharField(max_length=2, choices=(('1','sales'),('2','Delivery'),('3','Warehouse')), default=1)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.empno
+
+
+
